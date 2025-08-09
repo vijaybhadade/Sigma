6192514b9f331531d4352a962f3d9166e58ae232
vijaybhadade <vijaybhadade01@gmail.com>
2025-08-09 13:59:22 +0530

    this is chapter learn about query srting,path parameter,routing and express start and send response.

--- Day30/index.js
+++ Day30/index.js
@@ -1,12 +1,49 @@
-const express=require("express");
+const express = require("express");
 
-const app=express();
-let port=8080;
-app.listen(port,()=> {
-    console.log(`server started on port ${port}`);
+const app = express();
+let port = 8080;
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
 
-app.use((req,res)=> {
-    console.log("Request received",req.url);
-    res.send("Hello,World!");
+// Routes
+// app.get("/home", (req, res) => {
+//     res.send("Welcome to home page!");
+// });
+
+// app.get("/about", (req, res) => {
+//     res.send("Welcome to about page!");
+// });
+
+// app.get("/project", (req, res) => {
+//     res.send("Welcome to project page!");
+// });
+
+// app.post("/", (req, res) => {
+//     res.send("Welcome to root path!");
+// });
+
+console.log("this is path parameter(req.params)");
+app.get("/:username/:id",(req,res)=>{
+    let username=req.params.username;
+    let id=req.params.id;
+    res.send(`welcome to our account ${username} with ID ${id}`)
 })
+
+// Catch-all for ALL HTTP methods and unmatched routes
+// app.all(/.*/, (req, res) => {
+//     console.log("Request received", req.url);
+//     res.send("This path does not exist");
+// });
+
+console.log("this is query parameter(req.query)");
+app.get("/query", (req, res) => {
+    let searchTerm = req.query.search;
+    let color=req.query.color;
+    if(!searchTerm || !color){
+        res.send("<h1>Not searched any thing</h1>")
+    }
+    res.send(`You searched for: ${searchTerm} and color is ${color}`);
+});
